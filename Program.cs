69d777f9fe19b5b43cc3a209fe29c2257b91d66c
Eugene Krivinsky <silver313@mail.ru>
2024-01-16 17:33:59 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SilverSocialNetwork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
