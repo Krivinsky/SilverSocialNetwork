b2ed35cc71a7842f97d54bcb42ccc337a3427e82
Eugene Krivinsky <Playa313@yandex.ru>
2024-01-16 21:42:46 +0300

    добавлен слой бизнес логики

---
+++ BLL/Services/UserService.cs
@@ -0,0 +1,58 @@
+﻿using SilverSocialNetwork.BLL.Models;
+using SilverSocialNetwork.DAL.Entities;
+using SilverSocialNetwork.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SilverSocialNetwork.BLL.Services
+{
+    public class UserService
+    {
+        IUserRepository userRepository;
+        public UserService() 
+        {
+            userRepository = new UserRepository();
+        }
+
+        public void Register(UserRegistrationData userRegistrationData) 
+        {
+            if (string.IsNullOrEmpty(userRegistrationData.FirstName))
+                throw new ArgumentNullException();
+
+            if (string.IsNullOrEmpty(userRegistrationData.LastName))
+                throw new ArgumentNullException();
+
+            if (string.IsNullOrEmpty(userRegistrationData.Password))
+                throw new ArgumentNullException();
+
+            if (string.IsNullOrEmpty(userRegistrationData.Email))
+                throw new ArgumentNullException();
+
+            if (userRegistrationData.Password.Length < 8) 
+                throw new ArgumentOutOfRangeException();
+
+            if (!new EmailAddressAttribute().IsValid(userRegistrationData.Email))
+                throw new ArgumentException();
+
+            if (userRepository.FindByEmail(userRegistrationData.Email) != null)
+                throw new ArgumentException();
+
+            var userEntity = new UserEntity()
+            {
+                firstname = userRegistrationData.FirstName,
+                lastname = userRegistrationData.LastName,
+                password = userRegistrationData.Password,
+                email = userRegistrationData.Email,
+
+            };
+
+            if (this.userRepository.Create(userEntity) == 0)
+                throw new Exception();
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,60 @@
-﻿namespace SilverSocialNetwork
+﻿using SilverSocialNetwork.BLL.Models;
+using SilverSocialNetwork.BLL.Services;
+
+namespace SilverSocialNetwork
 {
     internal class Program
     {
+        public static UserService userService = new UserService();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("\t\t**** Welcome to SilverSocialNetwork *****");
+            //Console.WriteLine("для выхода вв");
+            while (true)
+            {
+                Console.WriteLine("Для регистрации введите имя пользователя:");
+                string firstName = Console.ReadLine();
+
+                Console.WriteLine("Фамилию");
+                string lastName = Console.ReadLine();
+
+                Console.WriteLine("почтовый адрес");
+                string email = Console.ReadLine();
+
+                Console.WriteLine("пароль");
+                string password = Console.ReadLine();
+
+                UserRegistrationData userRegistrationData = new UserRegistrationData()
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Password = password,
+                    Email = email
+                };
+
+                try
+                {
+                    userService.Register(userRegistrationData);
+                    Console.WriteLine("Регистраци произошла успешно ");
+                }
+                catch (ArgumentNullException ex)
+                {
+                    Console.WriteLine("Поля не могут быть пустыми");
+                    Console.WriteLine(ex.ToString);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine("Введено не корректное значение");
+                    Console.WriteLine(ex.ToString);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Произошла ошибка при регистрации");
+                    Console.WriteLine(ex.ToString);
+                }
+
+            }
+            Console.ReadLine();
         }
     }
 }
